1fc1146eb9db149b446b61bbeedc2031e7e5569e
aozora-asu <fromage.radix@gmail.com>
2024-12-02 14:04:03 +0900

    更新

---
+++ docs/whitepaper-summary/dist/assets/slidev/DrawingPreview-BBzbUWGN.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-C7dh46Y7.js";import"../modules/vue-vWtuqWGm.js";import"../index-CwZAdp1r.js";import"../modules/shiki-DU3eGCQ8.js";export{o as default};
--- docs/whitepaper-summary/dist/assets/slidev/DrawingPreview-CYiWUaiq.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-ZbwrMy-t.js";import"../modules/vue-vWtuqWGm.js";import"../index-BA4pPnDM.js";import"../modules/shiki-DU3eGCQ8.js";export{o as default};
